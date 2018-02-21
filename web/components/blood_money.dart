f4be94851decbf52338ae56886b0a7a5d7c28db0
Tobe O <thosakwe@gmail.com>
2018-02-21 16:43:42 -0500

    Basic landing page

--- web/components/blood_money.dart
+++ web/components/blood_money.dart
@@ -1,8 +1,16 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+import 'landing.dart';
 
 @Component(
     selector: 'blood-money',
-    template: '''
-<h1>NRA Blood Money</h1>
-''')
+    template: '<router-outlet></router-outlet>',
+    directives: const [ROUTER_DIRECTIVES])
+@RouteConfig(const [
+  const Route(
+    path: '/',
+    name: 'Landing',
+    component: LandingComponent,
+  ),
+])
 class BloodMoneyComponent {}
---
+++ web/components/landing.dart
@@ -0,0 +1,8 @@
+import 'package:angular/angular.dart';
+
+@Component(
+    selector: 'landing-page',
+    template: '''
+NRA Blood Money
+''')
+class LandingComponent {}
