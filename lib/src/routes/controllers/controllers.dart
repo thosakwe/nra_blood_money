028fde813934e36c72f28c63aaa166f3894824ac
Tobe O <thosakwe@gmail.com>
2018-02-21 16:26:11 -0500

    Project init

---
+++ lib/src/routes/controllers/controllers.dart
@@ -0,0 +1,8 @@
+library blood_money.src.routes.controllers;
+
+import 'dart:async';
+import 'package:angel_framework/angel_framework.dart';
+
+Future configureServer(Angel app) async {
+  /// Controllers will not function unless wired to the application!
+}
