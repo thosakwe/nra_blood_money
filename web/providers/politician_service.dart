7060331f8f5ca67cd5c40e2269e07e153d4f54e7
Tobe O <thosakwe@gmail.com>
2018-02-21 22:16:42 -0500

    Async load politicians

--- web/providers/politician_service.dart
+++ web/providers/politician_service.dart
@@ -10,18 +10,29 @@ class PoliticianService {
   final List<Politician> items = [];
   final Angel app;
   final Service service;
+  bool _loading = false;
 
-  PoliticianService(this.app) : service = app.service('api/politicians');
+  PoliticianService(this.app) : service = app.service('api/politicians') {
+    // Pre-load list of politicians asynchronously
+    load();
+  }
+
+  bool get loading => _loading;
 
   // TODO: Cache in localStorage
   Future load() => reload();
 
   Future reload() async {
-    Iterable<Politician> politicians =
-        await service.index().then((it) => it.map(Politician.parse));
-    this.items
-      ..clear()
-      ..addAll(politicians);
+    try {
+      _loading = true;
+      Iterable<Politician> politicians =
+          await service.index().then((it) => it.map(Politician.parse));
+      this.items
+        ..clear()
+        ..addAll(politicians);
+    } finally {
+      _loading = false;
+    }
   }
 
   Future<Politician> fetch(String id) =>
