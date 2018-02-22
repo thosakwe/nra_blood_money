171924441be9c98a2304c9a33423e5ec21dee01b
Tobe O <thosakwe@gmail.com>
2018-02-21 21:59:51 -0500

    Base politician service

---
+++ web/providers/politician_service.dart
@@ -0,0 +1,9 @@
+import 'package:angel_client/angel_client.dart';
+import 'package:angular/angular.dart';
+
+@Injectable()
+class PoliticianService {
+  final Angel app;
+
+  PoliticianService(this.app);
+}
