feffb4e8d3b76729000c64486412f701eea0abbb
Tobe O <thosakwe@gmail.com>
2018-02-21 22:01:06 -0500

    Dynamic base URL

--- web/main.dart
+++ web/main.dart
@@ -1,20 +1,26 @@
+import 'package:angel_client/browser.dart';
 import 'package:angel_configuration/browser.dart';
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'components/blood_money.dart';
 
 void main() {
+  String basePath;
   List providers = [
     ROUTER_PROVIDERS,
   ];
 
   if (config('debug')) {
     // Dev mode config
+    basePath = 'http://localhost:3000';
     providers.add(provide(LocationStrategy, useClass: HashLocationStrategy));
   } else {
     // Prod mode config
+    basePath = '/';
     providers.add(provide(APP_BASE_HREF, useValue: '/'));
   }
+
+  providers.add(provide(Angel, useValue: new Rest(basePath)));
 
   bootstrap(BloodMoneyComponent, providers);
 }
