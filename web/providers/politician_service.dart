b772478ad1116202ec9c4e02df758a30a58f713e
Tobe O <thosakwe@gmail.com>
2018-02-21 22:07:39 -0500

    Basic politician service, client-side

--- web/providers/politician_service.dart
+++ web/providers/politician_service.dart
@@ -1,9 +1,29 @@
+import 'dart:async';
+import 'dart:html';
 import 'package:angel_client/angel_client.dart';
 import 'package:angular/angular.dart';
+import 'package:blood_money/models.dart';
 
 @Injectable()
 class PoliticianService {
+  static const String _key = 'politicians';
+  final List<Politician> items = [];
   final Angel app;
+  final Service service;
 
-  PoliticianService(this.app);
+  PoliticianService(this.app) : service = app.service('api/politicians');
+
+  // TODO: Cache in localStorage
+  Future load() => reload();
+
+  Future reload() async {
+    Iterable<Politician> politicians =
+        await service.index().then((it) => it.map(Politician.parse));
+    this.items
+      ..clear()
+      ..addAll(politicians);
+  }
+
+  Future<Politician> fetch(String id) =>
+      service.read(id).then(Politician.parse);
 }
