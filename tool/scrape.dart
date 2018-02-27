74211d8d1703b160cfa2f65c2ffafe28fee8c921
Tobe O <thosakwe@gmail.com>
2018-02-26 20:04:12 -0500

    Scrape election+phone

--- tool/scrape.dart
+++ tool/scrape.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'package:angel_validate/angel_validate.dart';
 import 'package:blood_money/models.dart';
 import 'package:intl/intl.dart';
 import 'package:isolate/isolate_runner.dart';
@@ -20,6 +21,11 @@ final RegExp commaOrSpace = new RegExp(r'[, ]');
 final RegExp moneyValid = new RegExp(r'[0-9,.]+');
 
 final RegExp assumedOffice = new RegExp(r'[Aa]ssumed office');
+
+final RegExp email = new RegExp(
+    r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
+
+final RegExp phoneNumber = new RegExp(r'\([0-9]+\)\s*[0-9]+\s*-[0-9]+');
 
 var dateFormat = new DateFormat('MMMM d, yyy');
 
@@ -113,7 +119,7 @@ Future<List<Politician>> fetchFromCycle(int cycle) async {
       var now = new DateTime.now();
 
       var p = new Politician(
-        name: '${names[1]} ${names[0]}',
+        name: '${names[1]} ${names[0]}'.trim(),
         party: $tds[1].text == 'D' ? 'Democrat' : 'Republican',
         state: $tds[2].text,
         position: guessPosition($tds[3].text.trim()),
@@ -125,7 +131,7 @@ Future<List<Politician>> fetchFromCycle(int cycle) async {
 
       // Get rid of "three-name" situations
       if (names.length == 3) {
-        p.name = '${names[1]} ${names[0]}';
+        p.name = '${names[1]} ${names[0]}'.trim();
         //print('$names - ${names.length}');
       }
 
@@ -196,9 +202,12 @@ Future<Politician> scrapeWikipedia(Politician p, http.BaseClient client) async {
 
     p = await scrapeReelectionFromVcard(p, doc);
 
+    if (p != null) p = await scrapeEmailAndPhoneFromWebsite(p, client);
+
     return p;
-  } catch (e) {
-    print('Error scraping ${p.name}.');
+  } catch (e, st) {
+    print('Error scraping ${p.name}: $e');
+    print(st);
     var file = new File('scrape/${p.name}.html');
     await file.create(recursive: true);
     await file.writeAsString(response?.body);
@@ -229,7 +238,7 @@ Politician scrapeReelectionFromVcard(Politician p, html.Document doc) {
             var termStarted = dateFormat.parse(dateString);
             int termLength;
 
-            switch(p.position) {
+            switch (p.position) {
               case 'Representative':
                 termLength = 2;
                 break;
@@ -240,14 +249,14 @@ Politician scrapeReelectionFromVcard(Politician p, html.Document doc) {
                 termLength = 4;
                 break;
               default:
-                print('Cannot determine term length for ${p.name}, a ${p.position}.');
+                print('Cannot determine term length for ${p.name}, a ${p
+                    .position}.');
                 return null;
             }
 
             int reelectionYear = termStarted.year + termLength - 1;
 
-            while (reelectionYear <= now.year)
-              reelectionYear += termLength;
+            while (reelectionYear <= now.year) reelectionYear += termLength;
 
             p.election = new ElectionInfo(
               month: 11,
@@ -262,4 +271,44 @@ Politician scrapeReelectionFromVcard(Politician p, html.Document doc) {
   }
 
   return p;
+}
+
+Future<Politician> scrapeEmailAndPhoneFromWebsite(
+    Politician p, http.BaseClient client) async {
+  if (p.website == null) return p;
+
+  try {
+    // Go to website root, try to fetch email and phone
+    var response = await client.get(p.website);
+    var emailMatch = email.firstMatch(response.body);
+    var phoneMatch = phoneNumber.firstMatch(response.body);
+
+    if (emailMatch != null) p.email = emailMatch[0];
+
+    if (phoneNumber != null) p.phone = phoneMatch[0];
+
+    if (p.email == null || p.phone == null) {
+      // TODO: Find contact page, reliably
+      response =
+          await client.get(Uri.parse(p.website).replace(path: 'contact'));
+
+      if (response.statusCode >= 200 && response.statusCode < 400) {
+        var emailMatch = email.firstMatch(response.body);
+        var phoneMatch = phoneNumber.firstMatch(response.body);
+
+        if (emailMatch != null) p.email = emailMatch[0];
+
+        if (phoneNumber != null) p.phone = phoneMatch[0];
+      }
+    }
+
+    if (p.email != null) print('Email for ${p.name}: ${p.email}');
+
+    if (p.phone != null) print('Phone for ${p.name}: ${p.phone}');
+  } catch (e) {
+    // Remove broken website link
+    p.website = null;
+  } finally {
+    return p;
+  }
 }
